ce1acfaa692c3c5feec801688b7aeb55f3856391
VukasinVujic <vukasinvujic021@gmail.com>
2020-02-04 12:26:01 +0100

    back to show the table

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import './index.css';
-import App from './Api';
+// import App from './Api';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
